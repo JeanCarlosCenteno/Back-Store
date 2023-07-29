a965779cfd15486fed781f367100e1d1513e929c
JeanCarlosCenteno <CentenoJean13@gmail.com>
2023-07-29 14:21:04 -0600

    first commit

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,1 +1,12 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Product } from "./entities/product.entity";
 
+
+
+@Module({
+    imports: [TypeOrmModule.forFeature([Product])],
+    providers: [],
+    controllers: [],
+})
+export class ProductModule {}
